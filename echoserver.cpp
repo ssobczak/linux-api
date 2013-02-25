30c01cafc08f823736fe4b771e616714e235d2b5
Szymon Sobczak <szymon.sobczak@gmail.com>
2013-02-25 16:18:06 +0100

    refactor echo server

--- echoserver.cpp
+++ echoserver.cpp
@@ -12,3 +12,21 @@
 
 #include "echoserver.h"
 
+void EchoServer::on_new_client(int socket) {
+	std::cout << "Got a new client: " << socket << std::endl;
+}
+
+void EchoServer::on_sock_ready(int socket) {
+	char buff[50];
+	int len;
+	len = recv(socket, buff, 50, MSG_DONTWAIT);
+	if (len == -1) {
+		if (errno != EAGAIN && errno != EWOULDBLOCK) {
+			perror("failed to read from client socket");
+		}
+	} else if (len == 0) {
+		close_connection(socket);
+	} else {
+		std::cout << socket << ": " << std::string(buff, len) << std::flush;
+	}
+}
